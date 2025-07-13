59ca9d963816aaf6a5a89f0450b4bf9f746e4d68
Leene <marleenstueber@web.de>
2025-07-13 02:33:43 +0200

    Installed lodash

--- src/components/TileList.tsx
+++ src/components/TileList.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import _ from "lodash";
 import styled from "styled-components";
 import type { LinkTile } from "../types/LinkTile";
 import { defaultLinks } from "../data/defaultLinks";
 import { Tile } from "../components/Tile";
 
 export function TileList() {
+  /*category*/
+  const filtered_defaultLinks = _.filter(defaultLinks, { tab: "Start" });
+
+  console.log("hIER ", filtered_defaultLinks);
+  //console.log("Erster Titel:", filtered_defaultLinks[0]?.title);
+  //console.log("hIER " + defaultLinks[0].tab);
   return (
+    /*<Div>
+      {defaultLinks.map((link: LinkTile) => (
+        <Tile tile={link} key={link.id} />
+      ))}
+    </Div>*/
     <Div>
       {defaultLinks.map((link: LinkTile) => (
         <Tile tile={link} key={link.id} />
